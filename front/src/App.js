9018c89814a7d124d264ea934dd4dd9170b02299
Igor Siluianov <olvin.hh@gmail.com>
2019-11-02 17:43:53 +0300

    add some front code

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hackathon
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      response: null,
+    };
+  }
+
+  render() {
+    const getDataFromServer = () => {
+      fetch('http://localhost:8080/greeting')
+          .then((response) => {
+            console.log(response);
+          });
+    };
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>
+              Hackathon!
+            </h1>
+            <button
+                onClick={getDataFromServer} className="send-button">Press me!</button>
+            <h2>Data from server:</h2>
+            {this.state.response || '- no data -'}
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
