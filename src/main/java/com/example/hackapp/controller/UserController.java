26b0c60e665b2c86d82d731a6d011f5756b03d9d
Kseniia Shavonina <kshavonina@yandex.ru>
2019-11-02 18:05:40 +0300

    Add user controller

---
+++ src/main/java/com/example/hackapp/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.hackapp.controller;
+
+import com.example.hackapp.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @PostMapping(path = "/addUser", consumes = "application/json", produces = "application/json")
+    public User addMember(@RequestBody User user) {
+        System.out.println(user);
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+
+}
