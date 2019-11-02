b918803d9a34da995f06807e732e0d0a0dbb2a7e
Kseniia Shavonina <kshavonina@yandex.ru>
2019-11-02 16:12:00 +0300

    test

---
+++ src/main/java/com/example/hackapp/HackAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hackapp/HackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
